e00db512f69b9e23bd661e413fe8da6304350eb5
Matic Tribuson <matic.tribuson@gmail.com>
2022-11-18 11:13:46 +0100

    Spearate tracking code, use mixpanel for basic tracking

--- options/options.js
+++ options/options.js
@@ -1,18 +1,20 @@
-import { getSync, saveSync } from "../shared/storage.js";
-import { track, update } from "../shared/utils.js";
+import { getSync, saveSync } from "../shared/storage.js"
+import { track } from "../shared/tracker.js"
+import { update } from "../shared/utils.js"
 
 function saveCredentials() {
     const email = document.getElementById('email').value
     const apiKey = document.getElementById('api-key').value
     saveSync('credentials', { email, apiKey }).then(() => {
         track({
-            eventName: 'NEW_USER',
+            event: 'NEW_USER',
+            distinct_id: email.split('@')[0],
             identifier: email.split('@')[0],
         })
         document.getElementById('status').textContent = 'Credentials saved!'
         setTimeout(() => {
             update()
-            chrome.tabs.getCurrent(tab => chrome.tabs.remove(tab.id))
+            // chrome.tabs.getCurrent(tab => chrome.tabs.remove(tab.id))
         }, 500)
     })
 }
@@ -28,4 +30,4 @@ function loadCredentials() {
 }
 
 loadCredentials()
-document.getElementById('save').addEventListener('click', saveCredentials);
+document.getElementById('save').addEventListener('click', saveCredentials)
---
+++ shared/tracker.js
@@ -0,0 +1,29 @@
+const MIXPANEL_PROJECT_TOKEN = 'a61ea6c0c2d34a4760e65c00ac58edaa'
+const TRACK_URL_BASE = 'https://api-eu.mixpanel.com/'
+
+export async function track (data) {
+    const typeOfData = typeof data
+    if (typeOfData !== 'object') {
+        console.error(`Cannot track non objects, ${typeOfData} given.`)
+        return
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {accept: 'text/plain', 'content-type': 'application/json'},
+        body: JSON.stringify([{
+            event: 'forgetMeNot',
+            properties: {
+                token: MIXPANEL_PROJECT_TOKEN,
+                time: Date.now(),
+                ...data,
+            },
+        }]),
+    }
+
+    return fetch(`${TRACK_URL_BASE}track`, options)
+        .then(() => {
+            console.log('User signup tracking successful')
+        })
+        .catch(error => console.error(error))
+}
