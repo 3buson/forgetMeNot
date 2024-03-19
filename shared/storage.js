2eb825c367d4e4ef763c25517f0f1b80bdea866b
Dejan Levec <90892252+dejanceltra@users.noreply.github.com>
2024-03-19 15:13:40 +0100

    Remove hardcoded credentials

--- shared/storage.js
+++ shared/storage.js
@@ -1,7 +1,6 @@
 export const STORAGE_KEYS = {
     issueUpdatedTimestamps: "issueUpdatedTimestamps",
     lastChanged: "lastChanged",
-    credentials: "credentials",
 }
 
 export function saveLocally (key, value) {
@@ -11,18 +10,6 @@ export function saveLocally (key, value) {
 export function getLocally (key) {
     return new Promise(resolve => {
         chrome.storage.local.get().then(storage => {
-            resolve(storage[STORAGE_KEYS[key]])
-        })
-    })
-}
-
-export function saveSync (key, value) {
-    return new Promise(resolve => chrome.storage.sync.set({ [STORAGE_KEYS[key]]: value }, () => resolve()))
-}
-
-export function getSync (key) {
-    return new Promise(resolve => {
-        chrome.storage.sync.get().then(storage => {
             resolve(storage[STORAGE_KEYS[key]])
         })
     })
